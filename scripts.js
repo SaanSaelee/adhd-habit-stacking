85285ed59bbebb46c601b1d3834a770848b8b027
pennyflipper <saan.sinh.saelee@gmail.com>
2022-08-09 02:41:44 -0700

    updated ideas section with proper colors and padding

--- scripts.js
+++ scripts.js
@@ -5,7 +5,6 @@ let habit = [
   "Brush your teeth while showering.",
   "Have one toothbrush for the shower and one for the vanity.",
   "An electronic toothbrush with an app that tells you where you're brushing.",
-  "Have signage taped up as a reminder, a Redditor had mentioned a picture of tooth decay (think black lungs from smoking cigarettes).",
   "Brush with your partner or friend or roommate. Buddy system.",
   "Brush while on the toilet.",
   "Scroll through social media, TikTok, Insta, Twitter, and the usual suspects.",
@@ -16,15 +15,14 @@ let habit = [
   "Get active and spontaneous: dance around, hop up and down, stand on one leg, etc.",
   "Make it a ritual. Brush mindfully, slowly, tooth by tooth, section by section, focus on the bristles, etc.",
   "Reward yourself!",
-  "Fear factor: think about how bad it would be if you got cavities, a receding gum, tooth decay, the cost of having to get and pay for a root canal.",
   "Netflix. Catch up on your shows.",
   "Brush with the PokeMon Smile app. Everyone needs a little Pikachu in their lives. 😂",
 ];
 
-let ideaButton = document.getElementById("idea-button");
+let ideaButton = document.getElementById("ideas-btn");
 
 ideaButton.addEventListener("click", function () {
-  let idea = document.querySelector(".idea");
+  let idea = document.querySelector(".ideas");
   idea.textContent = habit[Math.floor(Math.random() * habit.length)];
 });
 // ideaButton.addEventListener("click", function () {
